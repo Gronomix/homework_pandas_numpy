dccc33829a87c9bd62bd3c26ddc6baca928dc2a8
Gronomix <Gronomix2013@gmail.com>
2023-04-11 09:38:47 +0300

    4 задание из pandas выполнено

--- task_4.py
+++ task_4.py
@@ -4,7 +4,8 @@
 
 import pandas as pd
 import numpy as np
-from pandas import DataFrame
+import xlwt
+
 
 
 df = pd.read_csv("6 class csv.csv")
@@ -16,4 +17,12 @@ print(df.groupby('Spectral Class').var())
 
 tar_tupe_var = df['Luminosity(L/Lo)'].var()
 print(tar_tupe_var)
+v_mean1 = df.groupby(['Temperature (K)', 'Luminosity(L/Lo)', 'Absolute magnitude(Mv)', 'Spectral Class']).std()
+print(df.groupby(['Temperature (K)', 'Luminosity(L/Lo)', 'Absolute magnitude(Mv)', 'Spectral Class']).std())
+v_mean_2 = str(df['Absolute magnitude(Mv)'].std())
+print(df['Absolute magnitude(Mv)'].std())
+v_mean_3 = df['Temperature (K)'].std()**2
+print(v_mean_3)
+
+df.to_excel('new_excel2.xlsx')
 
